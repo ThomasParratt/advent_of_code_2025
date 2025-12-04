19adf251fcc863000b9896e47c06ec575a9bfbb9
ThomasParratt <thomasparratt@googlemail.com>
2025-12-04 13:49:11 +0200

    done?

--- day1.cpp
+++ day1.cpp
@@ -5,12 +5,10 @@
 int get_amount(std::string str, int i)
 {
     std::string amount_str;
-    int amount_i = 0;
     int res = 0;
-    for (int idx = i; idx != '\n'; idx++)
+    for (int idx = i; std::isdigit(str[idx]); idx++)
     {
-        amount_str[amount_i] = str[idx];
-        amount_i++;
+        amount_str.push_back(str[idx]);
     }
     try 
     {
@@ -51,6 +49,7 @@ int get_result(std::string str)
 {
     int dial = 50;
     int amount;
+    int count = 0;
     for (int i = 0; i < str.length(); i++)
     {
         //std::cout << "str[i] = " << str[i] << std::endl;
@@ -60,6 +59,8 @@ int get_result(std::string str)
             amount = get_amount(str, i + 1);
             //std::cout << "amount = " << amount << std::endl;
             dial = move_left(dial, amount);
+            if (dial == 0)
+                count++;
             //std::cout << "dial = " << dial << std::endl;
         }
         if (str[i] == 'R')
@@ -68,18 +69,20 @@ int get_result(std::string str)
             amount = get_amount(str, i + 1);
             //std::cout << "amount = " << amount << std::endl;
             dial = move_right(dial, amount);
+            if (dial == 0)
+                count++;
             //std::cout << "dial = " << dial << std::endl;
         }
     }
-    return dial;
+    return count;
 }
 
 int main(void)
 {
-    std::ifstream file("test.txt");
+    std::ifstream file("input.txt");
     std::stringstream buffer;
     buffer << file.rdbuf();
 
-    std::cout << buffer.str() << std::endl << std::endl;
+    //std::cout << buffer.str() << std::endl << std::endl;
     std::cout << get_result(buffer.str()) << std::endl;
 }
